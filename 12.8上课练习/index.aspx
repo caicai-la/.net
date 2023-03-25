c3b001674bbd773f2c946a239c47c3462d0fc47d
caicai-la <117150712+caicai-la@users.noreply.github.com>
2023-03-25 20:21:46 +0800

    Add files via upload

---
+++ 12.8上课练习/index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="_12._8上课练习.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        性别：<asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="提交" OnClick="Button1_Click" />
+
+
+    </form>
+</body>
+</html>
---
+++ 12.8上课练习/index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _12._8上课练习
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //如果页面第一次加载值为false，
+            {
+                this.DropDownList1.Items.Add("男");  //Items：集合  add：方法
+                this.DropDownList1.Items.Add("女");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
