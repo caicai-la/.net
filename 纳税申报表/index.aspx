5280e756493e77d080addcdf654b0190d9a90fb8
caicai-la <117150712+caicai-la@users.noreply.github.com>
2023-03-25 20:29:27 +0800

    Add files via upload

---
+++ 纳税申报表/index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="纳税申报表.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        body{
+            text-align:center
+        }
+    </style>
+</head>
+<body>
+    <h2>纳税申报表</h2>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="姓&nbsp&nbsp名"></asp:Label><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text="地&nbsp&nbsp址"></asp:Label><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+             <asp:Label ID="Label3" runat="server" Text="职&nbsp&nbsp业"></asp:Label><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label4" runat="server" Text="工资总额"></asp:Label><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br /><br />
+            <asp:Button ID="Button1" runat="server" Text="提交" OnClick="Button1_Click" /><br /><br />
+            <asp:Label ID="Label5" runat="server" Text="申报信息"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 纳税申报表/index.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 纳税申报表
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var gongzi = Convert.ToInt32(this.TextBox4.Text);
+
+            if (gongzi <= 50000)
+            {
+                this.Label5.Text = $"{this.TextBox1.Text}您没有纳税!";
+            }
+            else if (gongzi > 50000 && gongzi <= 150000)
+            {
+                var biaozhun = gongzi * 0.2;
+                var nashuiShuoRu = gongzi - biaozhun;
+                var nashui = nashuiShuoRu * 0.2;
+                this.Label5.Text = $"{this.TextBox1.Text}您的纳税税款金额为：{nashui}";
+            }
+            else if (gongzi > 150000)
+            {
+                var biaozhun = gongzi * 0.2;
+                var xnashui = gongzi - biaozhun;
+                var nashuiShuoRu = gongzi - biaozhun;
+                var yfsk = nashuiShuoRu * 0.4;
+                this.Label5.Text = $"{this.TextBox1.Text}您的标准扣除额为：{biaozhun}、需纳税的收入为：{nashuiShuoRu}、应付税款为：{yfsk}";
+            }
+        }
+    }
+}
